b41677716fead1f64541d63a7d2db6f9d8aa7749
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-06 12:39:41 -0700

    switch script challenge 3

---
+++ codeCamp3.js
@@ -0,0 +1,7 @@
+// Write a switch statement to set answer for the following ranges:
+// 1-3 - "Low"
+// 4-6 - "Mid"
+// 7-9 - "High"
+//
+// Note
+// You will need to have a case statement for each number in the range.
