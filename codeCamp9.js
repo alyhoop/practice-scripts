38f107c25c25c5879ba8bf858afe295166934e96
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-07 16:36:21 -0700

    challenge 9 objects contd

--- codeCamp9.js
+++ codeCamp9.js
@@ -55,3 +55,29 @@ var myStorage = {
 var gloveBoxContents = myStorage.car.inside['glove box']; // Change this line
 
 <!--------------------Challenge---------------------------------------------->
+
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
+
+<!--------------------Challenge---------------------------------------------->
