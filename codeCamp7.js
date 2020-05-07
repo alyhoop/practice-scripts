fd6c0f68f038462666018f6a986cc682d8f1e142
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-07 13:40:08 -0700

    switch script challenge 7

--- codeCamp7.js
+++ codeCamp7.js
@@ -9,3 +9,44 @@ var testObj = {
 
 var playerNumber = 16;       // Change this line
 var player = testObj[playerNumber];   // Change this line
+
+<!---------------------------------Challenge----------------------------------->
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog.name = "Happy Coder";
+
+<!---------------------------------Challenge----------------------------------->
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog.bark = "woof";
+
+<!---------------------------------Challenge----------------------------------->
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+
+delete myDog.tails;
+
+<!---------------------------------Challenge----------------------------------->
