f4db2708ca8785e2c13b4ab07ca523d3daff8d5f
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-11 15:40:59 -0700

    codeWars arr challenge

--- codeCamp11.js
+++ codeCamp11.js
@@ -46,5 +46,22 @@ var myArr = [ 2, 3, 4, 5, 6];
 var total = 0;
 for (var i = 0; i < myArr.length; i++){
   total = total + myArr[i];
+  //total += myArr[i];
 }
+<!--------------------------------------------------------------------------->
+
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+for (var i=0; i < arr.length; i++){
+  for (var j=0; j <arr[i].length; j++) {
+    product = product * arr[i][j];
+  }
+}
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1,2],[3,4],[5,6,7]]);
+
 <!--------------------------------------------------------------------------->
