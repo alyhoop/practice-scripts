6bff2b7c249b9843dfe35039ffaa0aa0c4791147
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-11 14:58:36 -0700

    codeWars arr challenge

--- codeCamp11.js
+++ codeCamp11.js
@@ -7,3 +7,14 @@ while (i > -1) {
   myArray.push(i);
   i--;
 }
+
+<!--------------------------------------------------------------------------->
+
+// Setup
+var myArray = [];
+
+// Only change code below this line
+
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
