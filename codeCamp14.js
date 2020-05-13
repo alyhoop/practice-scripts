ae5c6109efc6fb9d286eb0b8eb9dd30ecc552656
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-13 15:50:32 -0700

    recursion

--- codeCamp14.js
+++ codeCamp14.js
@@ -80,3 +80,13 @@ function countdown(n){
 // Only change code above this line
 
 countdown(5);
+<!--------------------------------------------------------------------------->
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum - startNum === 0) {
+    return [startNum];
+  } else {
+    var numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+}
