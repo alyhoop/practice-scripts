3c3e7a4f0e8add6cf576ad73c011d153e463ec64
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-13 14:27:57 -0700

    parseInt

--- codeCamp14.js
+++ codeCamp14.js
@@ -29,3 +29,10 @@ function convertToInteger(str) {
 }
 
 convertToInteger("56");
+<!--------------------------------------------------------------------------->
+function convertToInteger(str) {
+ return parseInt(str, 2);
+}
+
+convertToInteger("10011");
+<!--------------------------------------------------------------------------->
