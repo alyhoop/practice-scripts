6609f697b7df03eb17694e732b1efdee648b409a
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-13 14:38:44 -0700

    ternary ops

--- codeCamp14.js
+++ codeCamp14.js
@@ -36,3 +36,11 @@ function convertToInteger(str) {
 
 convertToInteger("10011");
 <!--------------------------------------------------------------------------->
+
+function checkEqual(a, b) {
+return a === b ? 'Equal' : 'Not Equal';
+}
+
+checkEqual(1, 2);
+
+<!--------------------------------------------------------------------------->
