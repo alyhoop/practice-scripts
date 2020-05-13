3a23512ba73a820f6f9909832e347937bfa8bb5a
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-13 14:18:16 -0700

    modules

--- codeCamp14.js
+++ codeCamp14.js
@@ -23,3 +23,9 @@ function randomRange(myMin, myMax) {
   return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
   // Only change code above this line
 }
+<!--------------------------------------------------------------------------->
+function convertToInteger(str) {
+ return parseInt(str);
+}
+
+convertToInteger("56");
