6baff770773ad2672bde87ff9c48969d0e3f10ff
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-19 12:18:53 -0700

    es6

--- es6/es6_1.js
+++ es6/es6_1.js
@@ -306,3 +306,47 @@ const carrot = new Vegetable('carrot');
 console.log(carrot.name); // Should display 'carrot'
 
 <!---------------------------------------------------------------------------->
+/*Use getters and setters to Control Access to an Object*/
+
+// class Thermostat {
+//   constructor(fahrenheit) {
+//     this.fahrenheit = fahrenheit;
+//   }
+//
+//   get temperature() {
+//     return (5 / 9) * (this.fahrenheit - 32);
+//   }
+//
+//   set temperature(celsius) {
+//     this.fahrenheit = (celsius * 9.0) / 5 + 32;
+//   }
+//}
+
+//Getter/Setter are actually downvoted in MDN, use Object.defineProperty()
+
+<!---------------------------------------------------------------------------->
+/*Share code using a module script*/
+
+<html>
+  <body>
+    <!-- Only change code below this line -->
+<script type="module" src="index.js"></script>
+    <!-- Only change code above this line -->
+  </body>
+</html>
+
+<!---------------------------------------------------------------------------->
+/*Use export to share modules between files*/
+
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+
+export { uppercaseString, lowercaseString };
+
+
+<!---------------------------------------------------------------------------->
