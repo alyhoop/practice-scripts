a215c56075da67d41047f9f67dcc15a7cdff9210
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-20 12:41:32 -0700

    regex

--- regex/regex.js
+++ regex/regex.js
@@ -205,10 +205,3 @@ characters long, do not begin with numbers, and have two consecutive digits.*/
 let sampleWord = "astronaut";
 let pwRegex = /^\D(?=\w{5})(?=\w*\d{2})/; // Change this line
 let result = pwRegex.test(sampleWord);
-
-<!---------------------------------------------------------------------------->
-/*Checking for groups of characters using ()*/
-
-
-
-<!---------------------------------------------------------------------------->
