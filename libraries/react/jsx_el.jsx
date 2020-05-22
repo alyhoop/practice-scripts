4e339356077427b118b600ac029219fee1c221ab
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-22 15:41:24 -0700

    jsx comment

--- libraries/react/jsx_el.jsx
+++ libraries/react/jsx_el.jsx
@@ -7,7 +7,16 @@ const JSX =
   <p></p>
   <ul>
     <li></li>
+    {/*I am a comment*/}
     <li></li>
     <li></li>
   </ul>
 </div>
+
+const JSX = (
+  <div>
+    <h1>This is a block of JSX</h1>
+    {/*I am a comment*/}
+    <p>Here's a subtitle</p>
+  </div>
+);
