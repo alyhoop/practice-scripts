750e919ba172142097612c8255f9f4d92b8072dc
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-26 12:57:23 -0700

    self closing tags

---
+++ libraries/react/selfClosingTags.jsx
@@ -0,0 +1,17 @@
+// const JSX = (
+//   <div>
+//     {/* remove comment and change code below this line
+//     <h2>Welcome to React!</h2> <br >
+//     <p>Be sure to close all tags!</p>
+//     <hr >
+//     remove comment and change code above this line */}
+//   </div>
+// );
+
+const JSX = (
+  <div>
+    <h2>Welcome to React!</h2> <br />
+    <p>Be sure to close all tags!</p>
+    <hr />
+  </div>
+);
