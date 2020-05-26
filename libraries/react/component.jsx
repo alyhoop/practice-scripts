466fd89c3e9c41b0b0c6158ba3af0bfadbf3aeb5
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-26 13:08:11 -0700

    components

---
+++ libraries/react/component.jsx
@@ -0,0 +1,12 @@
+const MyComponent = function() {
+  // change code below this line
+return (<div className="text">
+  <p>
+      I  AM TEXT. SEE ME.
+  </p>
+</div>);
+
+
+
+  // change code above this line
+};
