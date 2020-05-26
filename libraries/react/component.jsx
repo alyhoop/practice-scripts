9d2cebd9ad1ab8d3d0f9e5b078a26af1a7406ba7
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-26 13:19:27 -0700

    components

--- libraries/react/component.jsx
+++ libraries/react/component.jsx
@@ -1,3 +1,5 @@
+//function name to start with capital letter
+
 const MyComponent = function() {
   // change code below this line
 return (<div className="text">
@@ -9,4 +11,23 @@ return (<div className="text">
 
 
   // change code above this line
+};
+
+//class entends component
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // change code below this line
+  return (
+  <div className="hi">
+    <h1>
+    Hello React!
+    </h1>
+  </div> );
+
+
+    // change code above this line
+  }
 };
