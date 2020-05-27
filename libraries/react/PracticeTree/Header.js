3f63edaf7d723a4d4b75ba87048e3e5db9962848
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-27 01:58:46 -0700

    practice tree

---
+++ libraries/react/PracticeTree/Header.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+const Header = props => <h1>This is my header yo.</h1>
+
+export default Header
---
+++ libraries/react/PracticeTree/MainContent.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const MainContent = props => <h1>Some main content yo</h1>
+
+export default MainContent
