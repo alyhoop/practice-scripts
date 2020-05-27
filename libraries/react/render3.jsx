9ee12f152440cc6d6cf0e752c8550e7690c0211f
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-26 22:23:02 -0700

    render

---
+++ libraries/react/render3.jsx
@@ -0,0 +1,22 @@
+// Objective: Fill in the boilerplate React code required to render an
+// unordered list (<ul>) to the page. The list should contain 3 list items
+// (<li>) with anything in them you want.
+
+// HINTS:
+// import the libraries you need first
+// use one of the libraries to render some JSX to the page
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+const JSX = (
+  <div>
+    <ul>
+      <li>this</li>
+      <li>is</li>
+      <li>list</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(JSX, document.getElementById("root"));
