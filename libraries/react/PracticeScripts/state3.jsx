77fa0e775f6c028c358a3e4d4796075f3e12522f
Alyssa Hooper <nesteralyssa@gmail.com>
2020-07-07 19:01:58 -0700

    React state practice files

--- libraries/react/PracticeScripts/state3.jsx
+++ libraries/react/PracticeScripts/state3.jsx
@@ -2,14 +2,17 @@ class MyComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Alyssa'
+      name: 'freeCodeCamp'
     }
   }
   render() {
+    // change code below this line
+const name = this.state.name;
+    // change code above this line
     return (
       <div>
         { /* change code below this line */ }
-          <h1>{this.state.name}</h1>
+        <h1>{name}</h1>
         { /* change code above this line */ }
       </div>
     );
