73c08c1484601b8d3eb28c876ebb7e1636fb08a0
Alyssa Hooper <nesteralyssa@gmail.com>
2020-07-30 11:28:39 -0700

    Updates coding challenges

---
+++ basic_js/codewars.js
@@ -0,0 +1,8 @@
+function filter_list(l) {
+ return l.filter( l => typeof l === "number");
+}
+
+
+//Test.assertSimilar(filter_list([1,2,'a','b']),[1,2])
+// Test.assertSimilar(filter_list([1,'a','b',0,15]),[1,0,15])
+// Test.assertSimilar(filter_list([1,2,'aasf','1','123',123]),[1,2,123])
