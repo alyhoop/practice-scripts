61b8743e1bfc964592c2523c7b8f676760fc8f13
Alyssa Hooper <nesteralyssa@gmail.com>
2020-08-17 10:00:54 -0700

    Fixes bug in sort kata

--- basic_js/sortKata.js
+++ basic_js/sortKata.js
@@ -2,6 +2,14 @@ function solution(nums){
   if (nums === null) {
     return []
   } else {
-    return nums.sort()
+    return nums.sort(nums.sort((a, b) => a - b)
   }
 }
+
+//Test
+describe("Solution", function() {
+  it("should test for something", function() {
+    Test.assertEquals(1 + 1, 2);
+    assert.strictEqual(1 + 1, 2);
+  });
+});
