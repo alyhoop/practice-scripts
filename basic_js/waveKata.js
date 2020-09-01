86224ec47a3dc6aafe092d6cd06071905cb2e927
Alyssa Hooper <nesteralyssa@gmail.com>
2020-09-01 11:09:23 -0700

    Adds practice files

---
+++ basic_js/waveKata.js
@@ -0,0 +1,16 @@
+//Task
+// ex. wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+function wave(text){
+  let finalArray = []
+for ( let i = 0; i < text.length; i++) {
+    let arr = text.split('')
+    if (arr[i] === ' ') {
+       continue;
+    }
+    arr[i] = arr[i].toUpperCase()
+
+    finalArray.push(arr.join(''))
+}
+    return finalArray
+}
