5df7615d7a18aa2805d7c2ac2cc9e9832d171ae2
Alyssa Hooper <nesteralyssa@gmail.com>
2020-09-04 15:02:10 -0700

    Adds SQL notes

--- SQL/file1.sql
+++ SQL/file1.sql
@@ -6,10 +6,31 @@ CRUD - create, read, update, delete - main db ops
 */
 
 /*
-Databases (db)             - collection of related info
-relational db (SQL)        - data into one or more tables
-non-relational  db (noSQL) - data into anything BUT a table.
-                         ex. graphs, key-value, etc
+Databases (db)                      - collection of related info
+
+relational db (SQL)                 - data into one or more tables
+                                    *mySQL manages this
+
+non-relational  db (noSQL)          - data as anything BUT a table.
+                                    *mongoDB manages this
+                                    ex. graphs, key-value, etc
+
+query                                - request to the dbms for info
+
+primary key                          - unique identifier for rows
+                                    ex. #, string, email, anything you want...
+
+surrogate key                        -has no value in the real world
+                                    ex. employee id no. 100
+
+natural key                          -key has mapping in the real world
+                                    ex. SSN
+
+foreign key                         -links row to another table
+
+composite key                       -key w/ two attributes; only together do they
+                                    uniquely identify each row
+
 */
 
 SELECT * FROM Customers;
