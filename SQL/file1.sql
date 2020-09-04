c26082fcb85b4d5f6b73996aceb9e006c3669b07
Alyssa Hooper <nesteralyssa@gmail.com>
2020-09-03 19:20:24 -0700

    Adds kata practice files

--- SQL/file1.sql
+++ SQL/file1.sql
@@ -1,3 +1,18 @@
+/*
+SQL                                 - language
+mySQL                               - database management system (dbms)
+database management system (dbms)   - software app that helps users create & maintain db
+CRUD - create, read, update, delete - main db ops
+*/
+
+/*
+Databases (db)             - collection of related info
+relational db (SQL)        - data into one or more tables
+non-relational  db (noSQL) - data into anything BUT a table.
+                         ex. graphs, key-value, etc
+*/
+
 SELECT * FROM Customers;
 
 SELECT DISTINCT Country FROM Customers;
+<></>
