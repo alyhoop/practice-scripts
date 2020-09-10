1ba3f94a3bcbb3309fe8bdc790c1c93a479138f3
Alyssa Hooper <nesteralyssa@gmail.com>
2020-09-09 17:01:35 -0700

    Adds object practice notes

--- basic_js/objects.js
+++ basic_js/objects.js
@@ -43,3 +43,71 @@ person['eyes'] = 'hazel';
 person.farewell = function() { alert("Bye everybody!"); }
 person.height
 person9.greeting()
+
+
+
+let cat = {
+  name : 'Bertie',
+  breed : 'Cymric',
+  color : 'black',
+  greeting: function() {
+    console.log('Meow!');
+  }
+}
+
+// Put your code here
+
+let catName = cat.name;
+cat.greeting();
+
+// Don't edit the code below here
+//
+// The cat's name is Bertie.
+//
+//The cat's color is black.
+
+
+
+let band = {
+    name: 'poop',
+    nationality: 'the states of poop',
+    genre: 'folk',
+    members: 'infinite',
+    formed: 2020,
+    split: false,
+    albums: [
+    {name: 'poop1', released: 2020},
+    {name: 'poo2', released: 2020}
+    ]
+  };
+
+
+  let bandInfo = 'My fav band is ' + band.name + ' they\'re from the ' + band.nationality + ', you should really check them out'
+
+
+// Don't edit the code below here
+
+let para1 = document.createElement('p');
+para1.textContent = bandInfo;
+section.appendChild(para1);
+
+let cat = {
+  name : 'Bertie',
+  breed : 'Cymric',
+  color : 'white',
+  greeting: function() {
+    console.log(`Hello, said ${cat.name} the ${cat.breed}`);
+  }
+}
+
+let cat2 = {
+  name : 'Fluffy',
+  breed : 'Fluff',
+  color : 'orange',
+  greeting: function() {
+    console.log(`Hello, said ${cat2.name} the ${cat2.breed}`);
+  }
+}
+
+cat.greeting()
+cat2.greeting()
